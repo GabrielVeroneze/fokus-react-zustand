dcd714d540973bdbda9c89a0e4464f2d09d4bfcc
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2025-12-11 16:11:09 -0300

    ✨ feat: integra BotaoModo com Zustand e adiciona lógica de seleção ativa
    Atualiza o componente `BotaoModo` para utilizar o estado global via `useCronometroStore`, permitindo alterar o modo do cronômetro ao clicar no botão. A classe de estilo agora é aplicada condicionalmente com base no modo ativo.

--- src/components/Cronometro/BotoesModos/BotaoModo/index.tsx
+++ src/components/Cronometro/BotoesModos/BotaoModo/index.tsx
@@ -1,22 +1,31 @@
+import { useCronometroStore } from '@/store'
+import type { ModoCronometro } from '@/types/ModoCronometro'
 import styles from './styles.module.css'
 
 interface BotaoModoProps {
     children: React.ReactNode
-    modoBotao: {
-        id: string
-        nome: string
-        frase: string[]
-        tempoInicialEmSegundos: number
-    }
+    modoBotao: ModoCronometro
 }
 
 export default function BotaoModo({ children, modoBotao }: BotaoModoProps) {
+    const modoCronometro = useCronometroStore((state) => state.modoCronometro)
+    const setModoCronometro = useCronometroStore(
+        (state) => state.setModoCronometro,
+    )
+
+    const ativo = modoBotao.id === modoCronometro.id
+
+    const handleClick = () => {
+        setModoCronometro(modoBotao)
+    }
+
     return (
         <button
             className={`
                 ${styles['cronometer-modes__button']}
-                ${styles['cronometer-modes__button--active']}
+                ${ativo ? styles['cronometer-modes__button--active'] : ''}
             `}
+            onClick={handleClick}
         >
             {children}
         </button>
