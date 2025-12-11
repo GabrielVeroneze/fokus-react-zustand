4328b3e04f5d8d60ce505be4114133e52361553f
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2025-12-11 15:17:11 -0300

    ✨ feat: adiciona modos de cronômetro e estado inicial do cronômetro
    Inclui a constante `MODO_CRONOMETRO` com os três modos (foco, descanso curto e descanso longo) e atualiza a store `useCronometroStore` para armazenar o modo selecionado, o tempo em segundos e a função de alteração do modo.

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,5 +1,39 @@
+import type { ModoCronometro } from '@/types/ModoCronometro'
 import { create } from 'zustand'
 
-interface CronometroState {}
+export const MODO_CRONOMETRO: Record<string, ModoCronometro> = {
+    FOCO: {
+        id: 'foco',
+        nome: 'Foco',
+        frase: ['Otimize sua produtividade,', 'mergulhe no que importa.'],
+        tempoInicialEmSegundos: 30,
+    },
+    DESCANSO_CURTO: {
+        id: 'descanso-curto',
+        nome: 'Descanso curto',
+        frase: ['Que tal dar uma respirada?', 'Faça uma pausa curta!'],
+        tempoInicialEmSegundos: 5,
+    },
+    DESCANSO_LONGO: {
+        id: 'descanso-longo',
+        nome: 'Descanso longo',
+        frase: ['Hora de voltar à superfície.', 'Faça uma pausa longa.'],
+        tempoInicialEmSegundos: 15,
+    },
+}
 
-export const useCronometroStore = create<CronometroState>((set) => ({}))
+interface CronometroState {
+    modoCronometro: ModoCronometro
+    tempoEmSegundos: number
+    setModoCronometro: (novoModo: ModoCronometro) => void
+}
+
+export const useCronometroStore = create<CronometroState>((set) => ({
+    modoCronometro: MODO_CRONOMETRO.FOCO,
+    tempoEmSegundos: MODO_CRONOMETRO.FOCO.tempoInicialEmSegundos,
+    setModoCronometro: (novoModo) =>
+        set({
+            modoCronometro: novoModo,
+            tempoEmSegundos: novoModo.tempoInicialEmSegundos,
+        }),
+}))
